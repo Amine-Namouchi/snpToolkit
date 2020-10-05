8ef90b50dce1b865a217ae7930658324092e790d
aminenamouchi <bioinfosuite@gmail.com>
2020-10-06 00:45:16 +0200

    update setup

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(name='snptoolkit',
       ],
       keywords='SNPs filtering annotation variants calling',
       install_requires=['pysam', 'pandas', 'tqdm', 'coloredlogs', 'Biopython','dash','plotly','numba'],
-      py_modules=['snpToolkit_modules'],
+      packages=['snpToolkit_modules'],
       scripts=['snptoolkit']
       )
