5ea3ed503b09aa7dbe89ccb7a754ba59baef5988
aminenamouchi <bioinfosuite@gmail.com>
2020-10-08 12:22:03 +0200

    update doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,8 @@ author = 'Amine Namouchi'
 extensions = [
 ]
 
+# The master toctree document.
+master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
