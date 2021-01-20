c18a98b80da5e8581b158cff94fc75e8f3c2e1a8
aminenamouchi <bioinfosuite@gmail.com>
2021-01-20 23:46:35 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(name='snptoolkit',
-      version='2.2.6',
+      version='2.2.7',
       description='SNPs filtering, annotation and combining',
       url='https://github.com/Amine-Namouchi/snpToolkit',
       author='Amine Namouchi',
       author_email='amine.namouchi@gmail.com',
       python_requires='>=3.0',
-      long_description='SNPs annotation, filtering and combining from vcf files',
+      long_description='Explore, filter, annotate and analyse your SNPs/indels from VCF files',
       license='GPLv3+',
       classifiers=[
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
